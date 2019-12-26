767e84b2d16f761b2b1de6414dac57642c3dc3d6
xull <528753732@qq.com>
2019-12-26 16:41:36 +0800

    第一天 完成header 底部tabbar 首页轮播图和九宫格

---
+++ src/components/tabbar/HomeContainer.vue
@@ -0,0 +1,108 @@
+<template>
+  <div>
+    <!-- 轮播图区域 -->
+    <mt-swipe :auto="4000">
+      <mt-swipe-item v-for="item in lunbotuList" :key="item.url">
+        没有借口，临时用顺序代替一下{{item}}
+        <img :src="item.img" alt />
+      </mt-swipe-item>
+    </mt-swipe>
+
+    <!-- 九宫格 -->
+    <ul class="mui-table-view mui-grid-view mui-grid-9">
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../images/menu1.png" alt="">
+          <div class="mui-media-body">新闻资讯</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../images/menu2.png" alt="">
+          <div class="mui-media-body">图片分享</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../images/menu3.png" alt="">
+          <div class="mui-media-body">商品购买</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+         <img src="../../images/menu4.png" alt="">
+          <div class="mui-media-body">留言反馈</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../images/menu5.png" alt="">
+          <div class="mui-media-body">视频专区</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+         <img src="../../images/menu6.png" alt="">
+          <div class="mui-media-body">联系我们</div>
+        </a>
+      </li>
+    </ul>
+
+    <h3>HomeCOntainer</h3>
+  </div>
+</template>
+
+<script>
+import { Toast } from "mint-ui";
+
+export default {
+  data() {
+    return {
+      lunbotuList: []
+    };
+  },
+  created() {
+    this.getLunbotu();
+  },
+  methods: {
+    getLunbotu() {
+      // this.$http.get("http://vue.studyit.io/api/getlunbo").then(result => {
+      //   if (result.body.status === 0) {
+      //     console.log("createsdddddd111111");
+      //     ths.lunbotuList = result.body.message;
+      //   } else {
+      //     Toast("获取轮播图失败");
+      //   }
+      // });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.mint-swipe {
+  //注意！！！！！！！！！！！！  这里是mint-swipe  不是上面的标签名字mt-swipe
+  height: 200px;
+  color: pink;
+  img {
+    width: 100%;
+    height: 100%;
+  }
+}
+
+.mui-grid-view.mui-grid-9 {
+    border: 0;
+    background: #fff;
+    img {
+      width: 60px;
+      height: 60px;
+    }
+    .mui-media-body {
+      font-size: 12px;
+    }
+  }
+
+  .mui-grid-view.mui-grid-9 .mui-table-view-cell {
+    border: 0;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,28 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+//导入 vue-resource
+import VueResource from 'vue-resource'
+Vue.use(VueResource)
+
 import './lib/mui/css/mui.css'
+import './lib/mui/css/icons-extra.css'
+
+//导入自己的router.js路由模块
+import router from './router.js'
 
 //导入app组件
 import app from './App.vue'
-import {Header} from 'mint-ui'
-Vue.component(Header.name,Header)
- 
+import { Header, Swipe, SwipeItem } from 'mint-ui'
+Vue.component(Header.name, Header)
+Vue.component(Swipe.name, Swipe)
+Vue.component(SwipeItem.name, SwipeItem)
+
 var vm = new Vue({
   el: '#app',
   data: {},
   methods: {},
+  router,
   render: e => e(app)
 });
